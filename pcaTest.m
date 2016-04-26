dfc867566027f7f5a331236e3a2f4be362ccaadf
wburton12 <wburton@mit.edu>
2016-04-26 14:28:41 -0400

    Tested Four PCA Schemes
    None of the four was obviously better, so we'll move forward with the
    simplest one.

--- pcaTest.m
+++ pcaTest.m
@@ -15,28 +15,85 @@ X = reshape(pwoa,basisSize,1024*1024);
 eigenImages = pca(X);
 meanImg = mean(X,1)';
 
-for imgToDisp = 20:30%1:basisSize
+corner1x = 400;
+corner1y = 550;
+corner2x = 650;
+corner2y = 750;
+maskx = [corner1x corner1x corner2x corner2x];
+masky = [corner1y corner2y corner2y corner1y];
+mask = poly2mask(maskx,masky,1024,1024); % Mask is 1 on the inside of the ROI, 0 outside.
+maskLin = reshape(mask,1024*1024,1);
+
+for imgToDisp = 1:10
     toImg = reshape(pwa(imgToDisp,:,:),1024*1024,1);
+    
+    % My method
     c = zeros(size(eigenImages,2),1);
     
     for i = 1:length(c)
         c(i) = (toImg-meanImg)'*eigenImages(:,i);
     end
     
-    absPCA = toImg./(eigenImages*c+meanImg);
+    estPWOA1 = (eigenImages*c+meanImg);
+    
+%     % Hiro's method
+%     Iminus = ~maskLin.*(toImg-meanImg);
+%     for i = 1:length(c)
+%         c(i) = Iminus'*eigenImages(:,i)./sqrt(1-(maskLin.*eigenImages(:,i))'*eigenImages(:,i));
+%     end
+%     estPWOA2 = (eigenImages*c+meanImg);
+    
+%     % Medley Method
+%     Iminus = ~maskLin.*(toImg-meanImg);
+%     for i = 1:length(c)
+%         c(i) = Iminus'*eigenImages(:,i)./(1-(maskLin.*eigenImages(:,i))'*eigenImages(:,i));
+%     end
+%     estPWOA3 = (eigenImages*c+meanImg);
+
+%     
+    % BEC 5 Method
+    estPWOA4 = meanImg+eigenImages*((toImg-meanImg)'*eigenImages)';
+    estPWOA4 = (toImg'*(~maskLin))./(estPWOA4'*(~maskLin))*estPWOA4;
+    
+    absPCA1 = toImg./estPWOA1;
     figure('units','normalized','outerposition',[0 0 1 1]);
     subplot(1,2,1);
-    imagesc(reshape(absPCA,1024,1024));
+    imagesc(reshape(absPCA1,1024,1024));
     load('MyColormaps','mycmap')
     colormap(mycmap);
     caxis([0 1.2]);
-    title('PCA');
+    title('PCA Mine');
     
+%     absPCA2 = toImg./estPWOA2;
+%     subplot(1,2,2);
+%     imagesc(reshape(absPCA2,1024,1024));
+%     load('MyColormaps','mycmap')
+%     colormap(mycmap);
+%     caxis([0 1.2]);
+%     title('PCA Hiro');
+    
+%     absPCA3 = toImg./estPWOA3;
+%     subplot(1,2,2);
+%     imagesc(reshape(absPCA3,1024,1024));
+%     load('MyColormaps','mycmap')
+%     colormap(mycmap);
+%     caxis([0 1.2]);
+%     title('PCA Medley');
+%     
+    absPCA4 = toImg./estPWOA4;
     subplot(1,2,2);
-    imagesc(reshape(absNoPCA(imgToDisp,:,:),1024,1024));
+    imagesc(reshape(absPCA4,1024,1024));
     load('MyColormaps','mycmap')
     colormap(mycmap);
     caxis([0 1.2]);
-    title('No PCA');
+    title('PCA BEC 5');
+    
+    
+%     subplot(1,2,2);
+%     imagesc(squeeze(absNoPCA(imgToDisp,:,:)));
+%     load('MyColormaps','mycmap')
+%     colormap(mycmap);
+%     caxis([0 1.2]);
+%     title('No PCA');
     
 end
